0ee65f5cf9b01b02906b625e623baf36c0975c51
siddiquah <siddiquahanjum111@gmail.com>
2023-11-16 18:30:57 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,121 @@
+
+const fs = require('fs');
+const https = require('https')
+const express = require('express');
+const app = express();
+const socketio = require('socket.io');
+app.use(express.static(__dirname))
+
+//we need a key and cert to run https
+//we generated them with mkcert
+// $ mkcert create-ca
+// $ mkcert create-cert
+const key = fs.readFileSync('cert.key');
+const cert = fs.readFileSync('cert.crt');
+
+const expressServer = https.createServer({key, cert}, app);
+
+const io = socketio(expressServer);
+
+expressServer.listen(8181);
+
+const offers = [
+    // offererUserName
+    // offer
+    // offerIceCandidates
+    // answererUserName
+    // answer
+    // answererIceCandidates
+];
+const connectedSockets = [
+    //username, socketId
+]
+
+io.on('connection',(socket)=>{
+    // console.log("Someone has connected");
+    const userName = socket.handshake.auth.userName;
+    const password = socket.handshake.auth.password;
+
+    if(password !== "x"){
+        socket.disconnect(true);
+        return;
+    }
+    connectedSockets.push({
+        socketId: socket.id,
+        userName
+    })
+
+    if(offers.length){
+        socket.emit('availableOffers',offers);
+    }
+    
+    socket.on('newOffer',newOffer=>{
+        offers.push({
+            offererUserName: userName,
+            offer: newOffer,
+            offerIceCandidates: [],
+            answererUserName: null,
+            answer: null,
+            answererIceCandidates: []
+        })
+        // console.log(newOffer.sdp.slice(50))
+        socket.broadcast.emit('newOfferAwaiting',offers.slice(-1))
+    })
+
+    socket.on('newAnswer', (offerobj, ackFunction) => {
+        console.log(offerobj);
+        const socketToAnswer = connectedSockets.find(s => s.userName === offerobj.offererUserName)
+        if(!socketToAnswer) {
+            return;
+        }
+        const socketIdToAnswer = socketToAnswer.socketId
+
+        const offerToUpdate = offers.find( o => o.offererUserName === offerobj.offererUserName)
+        if(!offerToUpdate) {
+            console.log("No offer to update");
+            return
+        }
+        ackFunction(offerToUpdate.offerIceCandidates)
+        offerToUpdate.answer = offerobj.answer
+        offerToUpdate.answererUserName = userName
+
+        socket.to(socketIdToAnswer).emit('answerResponse', offerToUpdate)
+    })
+
+    socket.on('sendIceCandidateToSignalingServer',iceCandidateObj=>{
+        const { didIOffer, iceUserName, iceCandidate } = iceCandidateObj;
+        // console.log(iceCandidate);
+        if(didIOffer){
+            //this ice is coming from the offerer. Send to the answerer
+            const offerInOffers = offers.find(o=>o.offererUserName === iceUserName);
+            if(offerInOffers){
+                offerInOffers.offerIceCandidates.push(iceCandidate)
+                // 1. When the answerer answers, all existing ice candidates are sent
+                // 2. Any candidates that come in after the offer has been answered, will be passed through
+                if(offerInOffers.answererUserName){
+                    //pass it through to the other socket
+                    const socketToSendTo = connectedSockets.find(s=>s.userName === offerInOffers.answererUserName);
+                    if(socketToSendTo){
+                        socket.to(socketToSendTo.socketId).emit('receivedIceCandidateFromServer',iceCandidate)
+                    }else{
+                        console.log("Ice candidate recieved but could not find answere")
+                    }
+                }
+            }
+        }else{
+            //this ice is coming from the answerer. Send to the offerer
+            //pass it through to the other socket
+            const offerInOffers = offers.find(o=>o.answererUserName === iceUserName);
+            const socketToSendTo = connectedSockets.find(s=>s.userName === offerInOffers.offererUserName);
+            if(socketToSendTo){
+                socket.to(socketToSendTo.socketId).emit('receivedIceCandidateFromServer',iceCandidate)
+            }else{
+                console.log("Ice candidate recieved but could not find offerer")
+            }
+        }
+        // console.log(offers)
+    })
+
+})
+
+
